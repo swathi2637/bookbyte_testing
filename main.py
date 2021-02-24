e86c07c8c54d962a7540ede70586f2e0872fc58d
@gudduri! <sgudduri@gmail.com>
2021-02-24 11:36:37 -0800

    Test case 2 and 3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import unittest
 from selenium import webdriver
+import requests
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class InputFormsCheck(unittest.TestCase):
@@ -7,6 +9,7 @@ class InputFormsCheck(unittest.TestCase):
     # Opening browser.
     def setUp(self):
         self.driver = webdriver.Chrome(r'C:\Users\swathi\Downloads\chromedriver\chromedriver')
+        wait = WebDriverWait(self.driver, 10)
 
     def test_singleInputField(self):
         pageUrl = "https://www.amazon.com/sp?seller=A2N51X1QYGFUPK"
@@ -19,6 +22,35 @@ class InputFormsCheck(unittest.TestCase):
         print(addressfromUI.text)
 
         self.assertEqual("2800 Pringle Rd SE Suite 100",addressfromUI.text,"Invalid address on UI")
+
+    def test_bookByteSearchBar(self):
+        pageUrl = "https://www.bookbyte.com/advancedsearch.aspx"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get(pageUrl)
+        input_box = driver.find_element_by_id("ctl00_ContentPlaceHolder1_tbKeywords")
+        # write search string
+        input_box.send_keys("college")
+        # click on search
+        search_button = driver.find_element_by_id('ctl00_ContentPlaceHolder1_ibSearch')
+        # sometime search button hidden behind cookies pop-up, so clicking it using java script
+        driver.execute_script("arguments[0].click();", search_button)
+        # get results -> number of records
+        number_of_record_menu = driver.find_elements_by_id("ctl00_ContentPlaceHolder1_lblTotalRecords")
+
+        self.assertTrue(len(number_of_record_menu) > 0, "Search is not working")
+
+    def test_googleBookSearch(self):
+        book_authors = ['Brian W. Kernighan', 'Dennis Ritchie']
+        book_url = 'https://www.googleapis.com/books/v1/volumes?q=isbn=013110362'
+        response = requests.request('GET', book_url)
+        json_response = response.json()
+        for data in json_response['items']:
+            if data['volumeInfo']['title'] == 'C Programming Language':
+                self.assertEqual(sorted(data['volumeInfo']['authors']), book_authors,
+                                 "C Programming Language authors not matched")
+
+
 
     # Closing the browser.
     def tearDown(self):
